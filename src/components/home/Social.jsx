b8386d3cdc7191bb2dbf3c927e9c4f48ffc7a8b4
Vision Jain <visionjain118@gmail.com>
2023-02-09 00:39:41 +0530

    animated photo and made social icons

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const social = () => {
   return (
     <div className="home__social">
-      <a href="https://www.instagram.com/__vision.jain__" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
+      <a href="https://www.instagram.com/__vision.jain__" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
 
-      <a href="https://twitter.com/_Vision_jain" className="home__social-icon" target="_blank"><i class="uil uil-uil uil-twitter"></i></a>
+      <a href="https://twitter.com/_Vision_jain" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-twitter-alt"></i></a>
 
-      <a href="https://www.github.com/visionjain" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
+      <a href="https://www.github.com/visionjain" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
     </div>
   )
 }
