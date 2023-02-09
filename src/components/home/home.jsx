413e8b331449ac09fd38de23206dbba4f3b84d56
Vision Jain <visionjain118@gmail.com>
2023-02-09 11:37:16 +0530

    added scroll button and function

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css"
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
@@ -17,6 +18,9 @@ const Home = () => {
 
 
         </div>
+
+        <ScrollDown />
+        
       </div>
     </section>
   )
