5ddda08acdba69642095d074d190eade52666200
Vision Jain <visionjain118@gmail.com>
2023-02-10 21:53:35 +0530

    added more socials

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -8,6 +8,10 @@ const social = () => {
       <a href="https://twitter.com/_Vision_jain" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-twitter-alt"></i></a>
 
       <a href="https://www.github.com/visionjain" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-github-alt"></i></a>
+
+      <a href="https://www.facebook.com/visionjain/" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-facebook-f"></i></a>
+
+      <a href="https://www.linkedin.com/in/vision-jain-560320222/" rel="noreferrer" className="home__social-icon" target="_blank"><i class="uil uil-linkedin-alt"></i></a>
     </div>
   )
 }
